6ca86e4068cd19aac3f2ee68f3b2fa959ad5b31e
Chuns Chen <i@chunschen.com>
2022-03-12 20:03:36 +0800

    feat: algolia docsearch

---
+++ src/components/search.tsx
@@ -0,0 +1,17 @@
+import { DocSearch, DocSearchTranslations } from '@docsearch/react';
+import { algolia } from 'config';
+
+const translations: DocSearchTranslations = {
+  button: {
+    buttonText: '搜索',
+    buttonAriaLabel: '搜索',
+  },
+};
+
+const Search = () => (
+  <div className="group">
+    <DocSearch {...algolia} translations={translations} />
+  </div>
+);
+
+export default Search;

8d2712992b2ffbeb83a92b45237f8ca729ca1581
Chuns Chen <i@chunschen.com>
2022-03-20 17:47:12 +0800

    chore: change date type

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -15,8 +15,8 @@ const Post = defineDocumentType(() => ({
     title: { type: 'string', required: false },
     keywords: { type: 'string', required: false },
     description: { type: 'string', required: false },
-    date: { type: 'date', required: false },
-    updated: { type: 'date', required: false },
+    date: { type: 'string', required: false },
+    updated: { type: 'string', required: false },
     link: { type: 'string', required: false },
   },
   computedFields: {

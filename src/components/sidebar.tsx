c66f6df9b25dad84ce74d8538ce6ef3e64d6eeb8
Chuns Chen <i@chunschen.com>
2022-04-07 21:07:50 +0800

    refactor: sidebar style

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -13,26 +13,31 @@ const File: FC<SideItem> = ({ id, text, link = '' }) => {
   const isExternal = isUrl(link);
   const selected = asPath.split('#')[0] === link;
   return (
-    <Link href={link}>
-      <a
-        {...(isExternal ? { target: '_blank', rel: 'noreferrer' } : {})}
-        className={`mb-1 block rounded-md p-2 text-sm ${
-          selected ? 'text-primary bg-secondary' : 'hover:bg-secondary'
-        }`}
-        onClick={() => {
-          menu.close();
-          window.sessionStorage.sidebarId = id;
-        }}
-      >
-        <span
-          className={`${selected && 'text-accent'}`}
-          dangerouslySetInnerHTML={{ __html: text }}
-        />
-        {isExternal && (
-          <ArrowUpRight className="ml-1 inline-block w-4 text-secondary" />
-        )}
-      </a>
-    </Link>
+    <div className="flex items-center">
+      <div className="w-6 flex-shrink-0">
+        <div className="ml-1 h-1 w-1 rounded-full bg-gray-500 bg-opacity-60" />
+      </div>
+      <Link href={link}>
+        <a
+          {...(isExternal ? { target: '_blank', rel: 'noreferrer' } : {})}
+          className={`block rounded-md text-sm transition-colors ${
+            selected ? 'text-primary' : 'hover:text-accent'
+          }`}
+          onClick={() => {
+            menu.close();
+            window.sessionStorage.sidebarId = id;
+          }}
+        >
+          <span
+            className={`${selected && 'font-medium text-accent'}`}
+            dangerouslySetInnerHTML={{ __html: text }}
+          />
+          {isExternal && (
+            <ArrowUpRight className="ml-1 inline-block w-3.5 text-tertiary" />
+          )}
+        </a>
+      </Link>
+    </div>
   );
 };
 
@@ -45,32 +50,34 @@ const Folder: FC<FolderProps> = (props) => {
   const { id, text, items, openMap, onClick } = props;
   const open = openMap[id] || false;
   return (
-    <>
+    <div>
       <div
-        className="mb-1 flex items-center rounded-md p-2 text-sm hover:bg-secondary"
+        className="flex cursor-pointer items-center rounded-md text-sm"
         onClick={() => onClick(id)}
       >
-        <ArrowRight
-          className={`mr-1 h-4 w-4 text-secondary ${
-            open ? 'rotate-90 transform' : ''
-          }`}
-        />
+        <div className="w-6 flex-shrink-0">
+          <ArrowRight
+            className={`h-3.5 w-3.5 transition-transform text-tertiary motion-reduce:transition-none ${
+              open ? 'rotate-90' : ''
+            }`}
+          />
+        </div>
         <span dangerouslySetInnerHTML={{ __html: text }} />
       </div>
-      <Collapse visible={open} className="pl-4">
-        {items?.map((i) => (
-          <Fragment key={i.id}>
-            {i?.items ? (
-              <>
+      <Collapse visible={open}>
+        <div className="ml-2 mt-4 flex flex-col space-y-4 border-l pl-2">
+          {items?.map((i) => (
+            <Fragment key={i.id}>
+              {i?.items ? (
                 <Folder {...i} openMap={openMap} onClick={onClick} />
-              </>
-            ) : (
-              <File key={i.link} {...i} />
-            )}
-          </Fragment>
-        ))}
+              ) : (
+                <File {...i} />
+              )}
+            </Fragment>
+          ))}
+        </div>
       </Collapse>
-    </>
+    </div>
   );
 };
 
@@ -109,17 +116,13 @@ const Sidebar = () => {
       onClose={menu.close}
       className="h-[calc(90vh-3.5rem)] w-full lg:sticky lg:top-14 lg:z-0 lg:h-[calc(100vh-3.5rem)] lg:w-72 lg:flex-shrink-0 lg:transform-none lg:rounded-none lg:border-r lg:transition-none"
     >
-      <aside className="h-full w-full select-none overflow-y-scroll p-4 lg:pl-0">
+      <aside className="flex h-full w-full select-none flex-col space-y-4 overflow-y-scroll p-4 lg:pl-0">
         {sidebar.map((i) => (
           <Fragment key={i.id}>
             {i.items ? (
-              <>
-                <Folder {...i} openMap={openMap} onClick={onToggle} />
-              </>
+              <Folder {...i} openMap={openMap} onClick={onToggle} />
             ) : (
-              <div className="ml-2">
-                <File key={i.link} {...i} />
-              </div>
+              <File key={i.link} {...i} />
             )}
           </Fragment>
         ))}

ac73554b259bc4499af32c698c5350dd440a8693
Chuns Chen <i@chunschen.com>
2022-04-15 20:39:01 +0800

    fix: toc and nav error

--- src/components/toc.tsx
+++ src/components/toc.tsx
@@ -90,7 +90,7 @@ const Toc = () => {
       title="本文目录"
       visible={toc.visible}
       onClose={toc.close}
-      className="right-0 max-h-[calc(90vh-3.5rem)] min-h-[50vh] w-full xl:sticky xl:top-14 xl:z-0 xl:h-full xl:max-h-full xl:w-72 xl:flex-shrink-0 xl:transform-none xl:transition-none"
+      className="right-0 h-[calc(90vh-3.5rem)] w-full xl:sticky xl:top-14 xl:z-0 xl:h-[calc(100vh-3.5rem)] xl:w-72 xl:flex-shrink-0 xl:transform-none xl:transition-none"
     >
       <div className="h-full w-full overflow-y-scroll p-4 lg:pr-0">
         <div className="mb-4 hidden font-medium md:block">{title}</div>
